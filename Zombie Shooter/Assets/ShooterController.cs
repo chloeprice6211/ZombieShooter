dc230edb64baa6c457edc9a84fbb1b7ec2eff49e
tim <1chloeprice@gmail.com>
2022-12-16 13:02:48 +0200

    raycast hit

--- Zombie Shooter/Assets/ShooterController.cs
+++ Zombie Shooter/Assets/ShooterController.cs
@@ -10,20 +10,35 @@ public class ShooterController : MonoBehaviour
     ThirdPersonController _thirdPersonController;
 
     [SerializeField] CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] LayerMask aimColliderMask;
+    public Transform debugTransform;
 
     private void Awake()
     {
         _input = GetComponent<StarterAssetsInputs>();
         _thirdPersonController = GetComponent<ThirdPersonController>();
     }
+
     void Update()
     {
         Aim();
+        HandleAimRaycast();
     }
 
     private void Aim()
     {
         bool pressed = _input.aim ? true : false;
         aimVirtualCamera.gameObject.SetActive(pressed);
+    }
+
+    void HandleAimRaycast()
+    {
+        Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(center);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f, aimColliderMask))
+        {
+            debugTransform.position = hit.point;
+        }
     }
 }
