1492598836cbf340907bb44a994aa2d8ef96115f
tim <1chloeprice@gmail.com>
2022-12-16 13:45:54 +0200

    aim

--- Zombie Shooter/Assets/ShooterController.cs
+++ Zombie Shooter/Assets/ShooterController.cs
@@ -13,6 +13,8 @@ public class ShooterController : MonoBehaviour
     [SerializeField] LayerMask aimColliderMask;
     public Transform debugTransform;
 
+    Vector3 _aimHitPoint = Vector3.zero;
+
     private void Awake()
     {
         _input = GetComponent<StarterAssetsInputs>();
@@ -21,24 +23,42 @@ public class ShooterController : MonoBehaviour
 
     void Update()
     {
+
+
         Aim();
         HandleAimRaycast();
     }
 
     private void Aim()
     {
-        bool pressed = _input.aim ? true : false;
-        aimVirtualCamera.gameObject.SetActive(pressed);
+        if (_input.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+
+            Vector3 worldAimTarget = _aimHitPoint;
+            worldAimTarget.y = transform.position.y;
+
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+        }
     }
 
     void HandleAimRaycast()
     {
+        _aimHitPoint = Vector3.zero;
+
         Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
         Ray ray = Camera.main.ScreenPointToRay(center);
 
         if (Physics.Raycast(ray, out RaycastHit hit, 1000f, aimColliderMask))
         {
             debugTransform.position = hit.point;
+            _aimHitPoint = hit.point;
         }
     }
 }
