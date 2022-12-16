6b522311379cdf30e3e12210d40c266416c13d9f
tim <1chloeprice@gmail.com>
2022-12-16 20:34:36 +0200

    removed treshhold

--- Zombie Shooter/Assets/Scripts/ThirdPersonController.cs
+++ Zombie Shooter/Assets/Scripts/ThirdPersonController.cs
@@ -196,7 +196,7 @@ namespace StarterAssets
         private void CameraRotation()
         {
             // if there is an input and camera position is not fixed
-            if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+            if (!LockCameraPosition)
             {
                 //Don't multiply mouse input by Time.deltaTime;
                 float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
